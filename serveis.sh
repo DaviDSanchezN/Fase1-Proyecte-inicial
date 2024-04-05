05d00bf4967d12d35c288021484df74453246a94
DaviDSanchezN <158029135+DaviDSanchezN@users.noreply.github.com>
2024-04-05 08:51:27 +0200

    serveis.sh

--- serveis.sh
+++ serveis.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-
 # Función para iniciar un servicio
 start_service() {
     echo "Iniciando el servicio $SERVICE en $IP..."
-    sshpass -p "$PASSWORD" ssh "$USER@$IP" "sudo systemctl start $SERVICE"
+    ssh -t "$USER@$IP" "sudo systemctl start $SERVICE"
 }
 
 # Función para detener un servicio
 stop_service() {
     echo "Deteniendo el servicio $SERVICE en $IP..."
-    sshpass -p "$PASSWORD" ssh "$USER@$IP" "sudo systemctl stop $SERVICE"
+    ssh -t "$USER@$IP" "sudo systemctl stop $SERVICE"
 }
 
 # Función para comprobar el estado de un servicio
 check_status() {
     echo "Comprobando estado del servicio $SERVICE en $IP..."
-    sshpass -p "$PASSWORD" ssh "$USER@$IP" "systemctl is-active --quiet $SERVICE && echo 'Estado: Funcionando' || echo 'Estado: Detenido'"
+    ssh -t "$USER@$IP" sudo systemctl status $SERVICE
 }
 
-# Main
+# Interfaz de usuario
+echo "¡Bienvenido al gestor de servicios!"
+echo "-----------------------------------"
+read -p "Por favor, introduzca su IP: " IP
+read -p "Por favor, introduzca su nombre de usuario: " USER
+read -p "Por favor, introduzca el nombre del servicio: " SERVICE
+read -p "Por favor, introduzca el comando que desea ejecutar (status/start/stop): " COMMAND
+
+# Ejecutar el script principal
 main() {
-    if [ "$#" -lt 5 ]; then
+    if [ "$#" -lt 4 ]; then
         echo "Uso: $0 <IP> <usuario> <contraseña> <comando> <servicio>"
         exit 1
     fi
 
     IP=$1
     USER=$2
-    PASSWORD=$3
-    COMMAND=$4
-    SERVICE=$5
+    COMMAND=$3
+    SERVICE=$4
 
     case $COMMAND in
         "status")
@@ -48,14 +54,5 @@ main() {
     esac
 }
 
-# Interfaz de usuario
-echo "¡Bienvenido al gestor de servicios!"
-echo "-----------------------------------"
-read -p "Por favor, introduzca su IP: " IP
-read -p "Por favor, introduzca su nombre de usuario: " USER
-read -sp "Por favor, introduzca su contraseña: " PASSWORD
-echo ""
-read -p "Por favor, introduzca el nombre del servicio: " SERVICE
+main "$IP" "$USER" "$PASSWORD" "$COMMAND" "$SERVICE"
 
-# Ejecutar el script principal
-main "$IP" "$USER" "$PASSWORD" "$@" "$SERVICE"
